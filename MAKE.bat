2d56c302b85ccf4f86ed676c90054726e69babb2
Dominik Madarász <zaklaus@outlook.com>
2023-09-27 09:01:42 +0200

    dont delete .art[00].zip

--- MAKE.bat
+++ MAKE.bat
@@ -613,7 +613,6 @@ rem if "!vis!"=="yes" echo !cc! !o! editor.exe  tools\editor\editor.c  !edit! !i
 rem !echo! editor       && !cc! !o! editor.exe  tools\editor\editor.c  !edit! !import! !args! || set rc=1
 rem !echo! editor2      && !cc! !o! editor2.exe tools\editor\editor2.c !edit!          !args! || set rc=1
 
-del .art[00].zip > nul
 !echo! v4k && !cc! engine\v4k.c !export! !edit! !args! || set rc=1
 
 if "!cc!"=="cl" (
