f9dec24eb5dc0c2fa49aa692bb3c9a779ab98da8
Dominik Madarász <zaklaus@outlook.com>
2024-04-17 13:05:53 +0200

    chg: --delta -> --fps

--- engine/split/v4k_main.c
+++ engine/split/v4k_main.c
@@ -52,7 +52,7 @@ static void v4k_post_init(float refresh_rate) {
     // preload brdf LUT early
     (void)brdf_lut();
 
-    uint64_t fps = optioni("--delta", 0);
+    uint64_t fps = optioni("--fps", 0);
     if( fps ) {
         window_fps_lock(fps);
     }
