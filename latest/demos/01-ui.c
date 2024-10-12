533fab02f91238a40a2b9ecff6131e1714be80f1
VOID SYNC <builder@v4.games>
2024-10-12 13:36:31 +0000

    Mirror sync 2024-10-12 13:36:31

--- latest/demos/01-ui.c
+++ latest/demos/01-ui.c
@@ -21,8 +21,6 @@ int main() {
     // load video
     video_t *v = video( "pexels-pachon-in-motion-17486489.mp4", VIDEO_RGB | VIDEO_LOOP );
 
-    static fbo_t main_fb;
-(void)main_fb;
     // app loop
     while( window_swap() ) {
         // input controls
